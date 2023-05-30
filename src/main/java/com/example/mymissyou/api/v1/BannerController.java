e67dd7fa8e1d3bd0ccd048fec731120042ac27ec
Tingkeng Wang <wangtk957@gmail.com>
2023-05-30 12:58:42 +0800

    feat: init part2

--- src/main/java/com/example/mymissyou/api/v1/BannerController.java
+++ src/main/java/com/example/mymissyou/api/v1/BannerController.java
@@ -1,2 +1,17 @@
-package com.example.mymissyou.api.v1;public class BannerController {
+package com.example.mymissyou.api.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class BannerController {
+    @GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "hello, 2221111sss1";
+    }
 }
