2c78bd7dc73325495aa2b339e9e4c280861cdea2
tk <wangtk957@gmail.com>
2023-08-23 20:31:12 +0930

    chore: add coupon-insert.sql & sku-insert.sql

---
+++ profile/sql/coupon-insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO coupon (id, title, start_time, end_time, minus, type, remark, whole_store)
+VALUES (NULL, '无门槛减0.1券', 1564956702000, 1943647908000, 0.1, 3, '全场无门槛立减', true);
---
+++ profile/sql/sku-insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO sku (id, price, discount_price, online, img, title, spu_id, category_id, root_category_id, specs, code, stock)
+VALUES
+    (NULL, 77.76, NULL, true, '', '金属灰·七龙珠', 2, 17, 3, '{"specs":[{"key_id":1,"key":"颜色","value_id":45,"value":"金属灰"},{"key_id":3,"key":"图案","value_id":9,"value":"七龙珠"},{"key_id":4,"key":"尺码","value_id":14,"value":"小号 S"}]}', '2$1-45#3-9#4-14', 5),
+
+    (NULL, 66, 59, true, '', '青芒色·灌篮高手', 2, 17, 3, '{"specs":[{"key_id":1,"key":"颜色","value_id":42,"value":"青芒色"},{"key_id":3,"key":"图案","value_id":10,"value":"灌篮高手"},{"key_id":4,"key":"尺码","value_id":15,"value":"中号 M"}]}', '2$1-42#3-10#4-15', 999),
+
+    (NULL, 88, NULL, true, '', '青芒色·圣斗士', 2, 17, 3, '{"specs":[{"key_id":1,"key":"颜色","value_id":42,"value":"青芒色"},{"key_id":3,"key":"图案","value_id":11,"value":"圣斗士"},{"key_id":4,"key":"尺码","value_id":16,"value":"大号  L"}]}', '2$1-42#3-11#4-16', 8);
